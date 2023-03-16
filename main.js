48ec1fa6c2205b20a4c6309516f4093a3e9e4f12
dokibacsi <kvalcsi02@gmail.com>
2023-03-16 23:22:01 +0100

    leptetesek 1x + kepcsere

--- main.js
+++ main.js
@@ -1,45 +1,86 @@
 window.addEventListener("load", () => {
   const CICASLISTA = [
     {
-      eleresiUt: "../kepek/cica1.jpg",
-      szoveg: "Nagyon cukkedli, bambán néző minicica",
+      eleresiUt: "../kepek/bagrol.jpg",
+      nev: "DJ Bagrol"
     },
     {
-      eleresiUt: "../kepek/cica6.jpg",
-      szoveg: "Nagyon cukkedli, aranyosan mosolygó cicuka",
+      eleresiUt: "../kepek/climo.jpg",
+      nev: "DJ Climo"
     },
     {
-      eleresiUt: "../kepek/cica7.jpg",
-      szoveg: "Nagyon cukkedli, őrülten mosolygó cicuka",
+      eleresiUt: "../kepek/djkaka.jpg",
+      nev: "DJ KAKA"
     },
     {
-      eleresiUt: "../kepek/cica2.jpg",
-      szoveg: "Nagyon cukkedli, kedvesen meglepődő micucicu",
+      eleresiUt: "../kepek/klimon.jpg",
+      nev: "DJ Klimon"
     },
     {
-      eleresiUt: "../kepek/cica4.jpg",
-      szoveg: "Nagyon cukkedli, aranyosan feltekintő micuka",
+      eleresiUt: "../kepek/knassix.jpg",
+      nev: "DJ Knassix"
+    },
+    {
+      eleresiUt: "../kepek/malos.jpg",
+      nev: "DJ Malos"
+    },
+    {
+      eleresiUt: "../kepek/przemooo.jpg",
+      nev: "DJ Przemooo"
+    },
+    {
+      eleresiUt: "../kepek/virgo.jpg",
+      nev: "DJ Virgo Nightbasse"
     },
   ];
-  const CONTAINER = document.querySelectorAll("article");
-  megjelenit(CICASLISTA, CONTAINER);
+  const ARTICLE = document.querySelectorAll("article");
+  fokepMegjelenit(CICASLISTA, ARTICLE)
+  megjelenit(CICASLISTA, ARTICLE);
   const KEPELEM = document.querySelectorAll("article .kepek img");
+  const ELOZO_ELEM = document.querySelectorAll("article #fokep #elozo");
+  const KOVETKEZO_ELEM = document.querySelectorAll("article #fokep #kovetkezo");
 
   /*Kattintásos eseménykezelő*/
   for (let index = 0; index < CICASLISTA.length; index++) {
-    KEPELEM[index].addEventListener("click", () => {
-      console.log(event.target.src); /*Ez az elem váltotta ki az eseményt.*/
-      const NAGYKEP = document.querySelectorAll("article #nagykep img"); /*Megfogja a kép helyét*/
+      if(KEPELEM){
+        KEPELEM[index].addEventListener("click", () => {
+        console.log(event.target.src); /*Ez az elem váltotta ki az eseményt.*/
+        const NAGYKEP = document.querySelectorAll("article #nagykep img"); /*Megfogja a kép helyét*/
       NAGYKEP[0].src = CICASLISTA[index].eleresiUt; /*Megváltoztatja a képet*/
-    });
+      console.log(index);
+      })};
+      if(ELOZO_ELEM){
+        ELOZO_ELEM[0].addEventListener("click", () => {
+        console.log(event.target.src);
+        const NAGYKEP = document.querySelectorAll("article #nagykep img");        
+        NAGYKEP[0].src = CICASLISTA[index].eleresiUt;
+        index--;
+      })};
+      if(KOVETKEZO_ELEM){KOVETKEZO_ELEM[0].addEventListener("click", () => {
+        console.log(event.target.src);
+        const NAGYKEP = document.querySelectorAll("article #nagykep img");
+        NAGYKEP[0].src = CICASLISTA[index + 1].eleresiUt;
+        index++;
+      })};
+    };
   }
-});
+);
+
+function fokepMegjelenit(lista, container){
+  let txt = "<div id='fokep'> <button id='elozo'>Balra</button>";
+  txt += `<div id="nagykep">
+        <img src=${lista[0].eleresiUt} alt=kep('FO')>
+        </div>`;
+  txt += "<button id='kovetkezo'>Jobbra</button>"
+  "</div>";
+  container[0].innerHTML += txt;
+}
 
 function megjelenit(lista, container) {
   let txt = "<div class='kepek'>";
   for (let index = 0; index < lista.length; index++) {
     txt += `<div>
-        <img src=${lista[index].eleresiUt} alt=cica(${index + 1})>
+        <img src=${lista[index].eleresiUt} alt=kep(${index + 1})>
         </div>`;
   }
   txt += "</div>";
